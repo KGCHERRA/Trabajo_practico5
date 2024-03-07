54fa8f476936b6fbb680bebdb8f529e5300dd69f
Gabriela Carvajal Herra <160189484+KGCHERRA@users.noreply.github.com>
2024-03-06 21:53:21 -0600

    Cambioss

--- main.py
+++ main.py
@@ -1,40 +1,54 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, status, HTTPException
+from fastapi.responses import JSONResponse
+import uuid
 
 app = FastAPI(
-    title="Trabajo_Practico5",
+    title="Trabajo_práctico5",
     version="0.0.1"
 )
-class User(BaseModel):
-    username: str
-    email: str
-    password: str
 
-usuarios_registrados = []
+users = {
+    "Hola1234": {
+        "username": "person1",
+        "name": "gabriela"
+    },
+    "Hola5678": {
+        "username": "person2",
+        "name": "sophia"
+    },
+}
 
-#1. Registro de Usuarios:
+# 1. Registro de Usuarios:
+
 @app.post("/api/v1/register")
-async def register_user(user: User):
-    usuarios_registrados.append(user)
-    return {"message": "El usuario esta registrado."}
+async def register_user(username: str, email: str, password: str):
+    user_id = str(uuid.uuid4())
+    users[user_id] = {
+        "username": username,
+        "email": email,
+        "password": password,
+        "Status_code": 201
+    }
 
+# 2. Obtener Datos de Usuario:
 
-
-#2. Obtener Datos de Usuario:
 @app.get("/user/{user_id}")
-async def get_user(user_id: int):
-    if user_id < 0 or user_id >= len(usuarios_registrados):
-        raise HTTPException(status_code=404, detail="No se encontro al usuario")
+async def get_user(user_id: str):
+    user = users.get(user_id)
+    if user:
+        return JSONResponse(
+            content=user,
+            status_code=status.HTTP_200_OK
+        )
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Este usuario no existe."
+        )
 
-    return usuarios_registrados[user_id]
 
-#3. Crear Tareas:
-
+# 3. Crear Tareas:
 
 
-#4. Listar Tareas por Usuario:
+# 4. Listar Tareas por Usuario:
 
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
