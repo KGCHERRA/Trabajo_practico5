ea1cb8fd3ef4d21e1f52806882260bc8ad2e0b15
Sophia Cespedes <sophiacespedes@Sophias-MacBook-Air.local>
2024-03-06 22:59:28 -0600

    Funciones de tareas agregadas

--- main.py
+++ main.py
@@ -10,14 +10,21 @@ class User(BaseModel):
     email: str
     password: str
 
+
+class Task(BaseModel):
+    title: str
+    description: str
+    status: str
+
+
 usuarios_registrados = []
+tareas_usuario = {}
 
 #1. Registro de Usuarios:
 @app.post("/api/v1/register")
 async def register_user(user: User):
     usuarios_registrados.append(user)
     return {"message": "El usuario esta registrado."}
-
 
 
 #2. Obtener Datos de Usuario:
@@ -29,11 +36,26 @@ async def get_user(user_id: int):
     return usuarios_registrados[user_id]
 
 #3. Crear Tareas:
+@app.post("/api/v1/tasks/create")
+async def create_task(user_id: int, task: Task):
+    if user_id < 0 or user_id >= len(usuarios_registrados):
+        raise HTTPException(status_code=404, detail="No se encontró al usuario")
+    if user_id not in tareas_usuario:
+        tareas_usuario[user_id] = []
 
-
+    tareas_usuario[user_id].append(task)
+    return {"message": "Tarea creada correctamente"}
 
 #4. Listar Tareas por Usuario:
+@app.get("/api/v1/tasks/{user_id}")
+async def get_tasks_by_user(user_id: int):
+    if user_id < 0 or user_id >= len(usuarios_registrados):
+        raise HTTPException(status_code=404, detail="No se encontró al usuario")
 
+    if user_id not in tareas_usuario or len(tareas_usuario[user_id]) == 0:
+        raise HTTPException(status_code=404, detail="No se encontraron tareas para este usuario")
+
+    return tareas_usuario[user_id]
 
 if __name__ == "__main__":
     import uvicorn
